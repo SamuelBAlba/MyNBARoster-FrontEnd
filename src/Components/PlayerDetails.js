20ef48486f1d3e44238d92769a288f23da5f9cca
SamuelBAlba <brayanbalba24@gmail.com>
2023-05-20 21:53:25 -0400

    player details

--- src/Components/PlayerDetails.js
+++ src/Components/PlayerDetails.js
@@ -38,14 +38,32 @@ export default function PlayerDetails() {
         <div className='details'>
 
             <div className='main'>
-            <img src={singlePlayer.img}/>
-            <h2>{singlePlayer.name} {singlePlayer.position} </h2>
-            <p>is active</p>
+                <div className='left'>
+                <img src={singlePlayer.img}/>
+                <h2>{singlePlayer.name} - {singlePlayer.position} </h2>
+                <p>is active</p>
+                </div>
+
+                <div className='right'>
+                    <ol>
+                        <li>Player is a scorred</li>
+                        <li>player is a passer</li>
+                    </ol>
+
+                </div>
+
+
             </div>
 
-
-
-
+            <div className='attributes'>
+                <p>Shooting: {singlePlayer.shooting}</p>
+                <p>Passing: {singlePlayer.passing}</p>
+                <p>Dribble: {singlePlayer.dribble}</p>
+                <p>Dunking: {singlePlayer.dunking}</p>
+                <p>Defense: {singlePlayer.defense}</p>
+                <p>Rebound: {singlePlayer.rebound}</p>
+                <p>Steal: {singlePlayer.steal}</p>
+            </div>
         </div>
     )
 }
